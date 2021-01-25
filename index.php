1cadff814c1a2c0e68eac271c4f6d8cd2dd33bd6
fincharry27 <harry.gfinch@gmail.com>
2021-01-25 13:47:19 +0000

    Changed connection method

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
 
-$host = 'dundata-mysqldbserver.mysql.database.azure.com';
-$db   = 'dundatadatabase';
-$user = 'mysqldbuser@dundata-mysqldbserver';
-$pass = 'dundata123!';
-$port = "3306";
-$charset = 'utf8mb4';
-
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$dsn = "mysql:host=$host;dbname=$db";
-try {
-     $pdo = new \PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+require_once('conn.php')
 echo 'Current PHP version: ' . phpversion();
 echo "SUP NERDS!";
 ?>
