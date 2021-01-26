1185a09a19f9429de3ebbb0dbae00b9acffda90c
fincharry27 <harry.gfinch@gmail.com>
2021-01-26 11:44:30 +0000

    renamed export to exportJSON

---
+++ Software_Code/exportJSON.php
@@ -0,0 +1,22 @@
+<?php  
+	require('conn.php');
+
+	$sql = "SELECT * FROM answer";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	$result = $stmt->fetchAll();
+
+
+
+	$array1 = array();
+	foreach ($result as $row) 
+	{
+		$array1[] = $row;
+	}
+
+
+	echo json_encode($array1);
+
+	header("Content-type: application/json");
+	header("Content-Disposition: attachment; filename=testjson.json");
+?>
