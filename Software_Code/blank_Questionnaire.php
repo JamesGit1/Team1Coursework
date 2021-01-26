a89d498930c9850c3f21b0290a0dbc4475b06412
Jesse <jessechan10@gmail.com>
2021-01-26 14:31:10 +0000

    Update blank_Questionnaire.php
    Co-Authored-By: lukedawe <43144118+lukedawe@users.noreply.github.com>

--- Software_Code/blank_Questionnaire.php
+++ Software_Code/blank_Questionnaire.php
@@ -8,15 +8,13 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
+<nav class="navbar navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+  </div>
+</nav>
 
 <body>
-
-  <nav class="navbar navbar-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Home</a>
-    </div>
-  </nav>
-
   <div class="container">
     <div class="row">
       <form>
@@ -64,19 +62,16 @@
 <script>
   var questionNumber = 1;
   var addCols = function(num, type) {
-      for (var i=1;i<=num;i++) {
     if (type == "text") {
       var myCol = $('<div class="card" id = ' + questionNumber +' "><div class="card-body"><input type = "text" id =' + questionNumber +
         ' placeholder="Enter question here:" class="card-title question-title"/><p class="card-text"><em>Answer here</em></p><div class="d-grid gap-2 d-md-flex justify-content-md-end"><a href="#"  id = "btnGen" class="btn btn-primary fas fa-check"></a><a href="#" class="btn btn-primary btn-danger fas fa-trash"></a></div><div class="form-check"><input type="checkbox" class="form-check-input" id="required"><label class="form-check-label" for="required">Required</label></div>    </div>   </div>'
       );
-
     } else if (type == "radio") {
       alert("Radio");
         var myCol = $('<div class="card" id = ' + questionNumber +' "><div class="card-body">  <input type = "text" id = "myText" placeholder="Enter question here:" class="card-title question-title"/>  <div id="contentPanel">  </div> <a href="#" onclick="addExtraRadio()" id = "btnGen" class="btn btn-primary btn-success fas fa-plus" id="addOption"></a>  <div class="d-grid gap-2 d-md-flex justify-content-md-end"><a href="#" class="btn btn-primary fas fa-check"></a><a href="#" class="btn btn-primary btn-danger fas fa-trash"></a>  </div><div class="form-check"><input type="checkbox" class="form-check-input" id="required"><label class="form-check-label" for="required">Required</label></div></div></div>');
     }
     myCol.appendTo('#questionPanel');
     questionNumber++;
-  }
   };
 
   function addExtraRadio() {
