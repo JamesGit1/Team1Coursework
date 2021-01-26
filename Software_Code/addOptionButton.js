fbafc77c18b5b4a0bf7e29ed12140382ce3f66c5
Jesse <jessechan10@gmail.com>
2021-01-26 13:45:11 +0000

    Testing CORS error

--- Software_Code/addOptionButton.js
+++ Software_Code/addOptionButton.js
@@ -1,6 +1,34 @@
-button = document.getElementById("addOption");
-// 3. Add event handler
-button.addEventListener ("click", function() {
-    document.write("<p className='card-text'><input type='text' id='option' placeholder='Enter option here...' " +
-        "className='card-title option'/></p>");
+var numberOfOptions = 1;
+var addCols = function (num){
+    for (var i=1;i<=num;i++) {
+        var div = $('<div class="cardParent"></div>');
+        var myCol = $('<p class="card-text"><input type = "text" id =' + numberOfOptions + ' placeholder="Enter option here..." class="card-title option"/></p>\n');
+        var deleteButton = $('<a href="#" class="btn btn-primary btn-secondary fas fa-times btn-delete close" style="margin-left: 20px;"></a>');
+        deleteButton.appendTo(myCol);
+        myCol.appendTo(div);
+        div.appendTo('#contentPanel');
+        numberOfOptions++;
+    }
+
+    $('.close').on('click', function(e){
+        e.stopPropagation();
+        var $target = $(this).parents('.cardParent');
+        $target.hide('slow', function(){ $target.remove(); });
+    });
+};
+
+$(document).ready(function(){
+    addCols('3');
+    return false;
+});
+
+$("#btnGen").click(function() {
+    addCols('1');
+    return false;
+});
+
+$("#deleteCard").click(function() {
+    var $target = $(this).parents('.card');
+    $target.hide('slow', function(){ $target.remove(); });
+    return false;
 });
