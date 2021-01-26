31a3ed551c5331567cbbbdb2a76e302e18bef81a
fincharry27 <harry.gfinch@gmail.com>
2021-01-26 11:29:16 +0000

    refactored to prompt json download

--- Software_Code/export.php
+++ Software_Code/export.php
@@ -6,28 +6,17 @@
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 
+
+
+	$array1 = array();
+	foreach ($result as $row) 
+	{
+		$array1[] = $row;
+	}
+
+
+	echo json_encode($array1);
+
+	header("Content-type: application/jsonv");
+	header("Content-Disposition: attachment; filename=testjson");
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Export data</title>
-</head>
-<body>
-	<?php
-		$array1 = array();
-		foreach ($result as $row) 
-		{
-			$array1[] = $row;
-		}
-
-		$fp = fopen('empdata.txt', 'w');
-		fwrite($fp, json_encode($array1));
-		fclose($fp);
-	?>
-	<button name="download">Download</button>
-</body>
-</html>
-
-
-present json in php and change headers to download automatically
