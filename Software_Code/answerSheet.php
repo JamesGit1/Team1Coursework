33116ad8836197bfc9fe97b04a7db0a6e3670b25
Luke <luke.dawe@gmail.com>
2021-01-28 18:47:33 +0000

    Started working on getting the options questions to work.

--- Software_Code/answerSheet.php
+++ Software_Code/answerSheet.php
@@ -25,332 +25,85 @@ $questions = $stmt->fetchAll();
 
 unset($stmt);
 ?>
-    <link rel="stylesheet" href="questionAppearance.css">
-    <!DOCTYPE html>
-    <html lang="en" dir="ltr">
+<link rel="stylesheet" href="questionAppearance.css">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-              crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-    <!--Navigation bar -->
-    <nav class="navbar navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Home</a>
-        </div>
-    </nav>
+<!--Navigation bar -->
+<nav class="navbar navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Home</a>
+    </div>
+</nav>
 
 <body>
 <div class="container" id="myDiv">
     <div class="row">
-    <form name="quiz" method="POST" action="submitAnswers.php">
-        <div id="questionPanel">
-            <?php
-            echo $title;
-            echo $description;
-            $questionArray=array();
-            foreach ($questions as $row) {
+        <form name="quiz" method="POST" action="submitAnswers.php">
+            <div id="questionPanel">
+                <?php
+                echo $title;
+                echo $description;
+                $questionArray = array();
+                foreach ($questions
+
+                as $row) {
+                if ($row['Type'] == 'text') {
+                    ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <p><?php echo $row['Contents'];
+                                if ($row['required'] == 1) {
+                                    echo "*";
+                                } ?></p>
+                            <!-- do we need ID here or question number? -->
+                            <input class="card-text" placeholder="Participant will answer here..."
+                                   name="<?php echo $row['ID'] ?>"/>
+                        </div>
+                    </div>
+                    <?php
+                } else if ($row['Type'] == 'radio') {
                 ?>
                 <div class="card">
                     <div class="card-body">
-                        <p><?php echo $row['Contents']; if($row['required']==1){echo"*";}?></p>
-                        <input class="card-text" placeholder="Participant will answer here..."
-                               data-quNumber = "<?php echo $row['question number']?>"
-                               name= "<?php echo $row['question number']?>"/>
+                        <p>
+                            <?php echo $row['Contents'];
+                            if ($row['required'] == 1) {
+                                echo "*";
+                            } ?></p>
+                        <?php
+                        // then we need to read out all the options that is in the questionnaire
+                        $query = "SELECT * FROM options o where o.`question ID` = :questionID";
+                        $stmt = $pdo->prepare($query);
+                        $stmt->bindParam(":questionID", $row['ID'], PDO::PARAM_STR);
+                        $stmt->execute();
+                        $options = $stmt->fetchAll();
+                        unset($stmt);
+                        foreach ($options as $option) {
+                            ?>
+                            <input type="radio" name="<?php echo $row['Contents']; ?>"
+                                   value=<?php echo $option['option']; ?>/>
+                            <?php
+                        }
+                        }
+                        array_push($questionArray, $row['ID']);
+                        }
+                        $_SESSION['questionArray'] = $questionArray;
+                        ?>
+                        <button type="submit" name="submitQuestion" class="btn btn-primary"></button>
                     </div>
                 </div>
-                <?php
-                array_push($questionArray, $row['question number']);
-            }
-            $_SESSION['questionArray'] = $questionArray;
-            ?>
-            <button type="submit" name="submitQuestion" class="btn btn-primary"></button>
-    </form>
-<?php
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            </div>
+        </form>
+    </div>
+</div>
+</body>
--- Software_Code/submitAnswers.php
+++ Software_Code/submitAnswers.php
@@ -7,6 +7,7 @@ echo "ALL POST VARIABLES";
 var_dump($_POST);
 echo "ALL SESSION VARIABLES";
 var_dump($_SESSION);
+echo "THANKS FOR FILLING IN THE FORM";
 if (isset($_POST['submitQuestion'])) {
     for ($i = 0; $i < sizeof($questionArray); $i++) {
         // then we need to read out all the questions that is in the questionnaire
@@ -15,12 +16,16 @@ if (isset($_POST['submitQuestion'])) {
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(":contents", $contents, PDO::PARAM_STR);
         $stmt->bindParam(":questionID", $questionID, PDO::PARAM_STR);
+        // TODO this needs to be fixed to have the ID of the participant, for now it will be 3
         $stmt->bindParam(":participantID", $participantID, PDO::PARAM_STR);
 
-        $contents = $_POST['contents'];
-        $questionID = $questionArray[i];
+        // here, we want the name of the variable $questionArray[$i] so that we can get the value of that
+        // from $_POST
+        $contents = $_POST[$questionArray[$i]];
+        $questionID = $questionArray[$i];
+        $participantID = 1;
 
-            $stmt->execute();
+        $stmt->execute();
         $questions = $stmt->fetchAll();
 
         unset($stmt);
