928800e8ae6060e083245c8e765e5814b978805e
Luke <luke.dawe@gmail.com>
2021-01-28 16:24:25 +0000

    Made a start on the answer sheet.

---
+++ Software_Code/answerSheet.php
@@ -0,0 +1,72 @@
+<?php
+require_once('conn.php');
+session_start();
+$id = $_SESSION['id'];
+
+// we want to find the questionnaire that we need to read from
+$query = "SELECT * FROM Questionnaire WHERE Questionnaire.id = :questionnaireID";
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(":questionnaireID", $questionnaireID, PDO::PARAM_STR);
+$questionnaireID = $id;
+$stmt->execute();
+$result = $stmt->fetchAll();
+foreach ($result as $row) {
+    $title = $row['name'];
+    $description = $row['description'];
+}
+unset($stmt);
+
+// then we need to read out all the questions that is in the questionnaire
+$query = "SELECT * FROM question q where q.`questionnaire id` = :questionnaireID";
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(":questionnaireID", $id, PDO::PARAM_STR);
+$stmt->execute();
+$questions = $stmt->fetchAll();
+
+unset($stmt);
+?>
+    <link rel="stylesheet" href="questionAppearance.css">
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+              crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <!--Navigation bar -->
+    <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">Home</a>
+        </div>
+    </nav>
+
+<body>
+<div class="container" id="myDiv">
+    <div class="row">
+    <form name="quiz" method="POST">
+        <div id="questionPanel">
+            <?php
+            foreach ($questions as $row) {
+                ?>
+                <div class="card">
+                    <div class="card-body">
+                        <p><?php echo $row['Contents'] ?></p>
+                        <input class="card-text" placeholder="Participant will answer here..."
+                               name= <?php echo $row['question number'] ?>/>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+            <button type="submit" name="submit question" class="btn btn-primary"></button>
+    </form>
+<?php
+
+if (isset($_POST['submit question'])) {
+
+}
