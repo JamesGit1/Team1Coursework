c624228792f80bf14f46a089927f6694f0f4189f
fincharry27 <harry.gfinch@gmail.com>
2021-01-28 14:21:04 +0000

    correct and working method (unlike jesse's)

--- Software_Code/blank_Questionnaire.php
+++ Software_Code/blank_Questionnaire.php
@@ -14,8 +14,26 @@
         $title = $row['name'];
         $description = $row['description'];
     }
+    unset($stmt);
 
+    $query = "SELECT * FROM question q where q.`questionnaire id` = 2";
+    $stmt = $pdo->prepare($query);
+    //$stmt->bindParam(":questionnaireID", $questionnaireID, PDO::PARAM_STR);
+    $questionnaireID = $id;
+    $stmt->execute();
+    $questions = $stmt->fetchAll();
 
+    unset($stmt);
+
+    if(isset($_POST['submitQuestions']))
+    {
+        $query = "INSERT INTO `question`(`Contents`,`Type`,`questionnaire ID`,`question number`,`required`) VALUES (:contents,'text', 2, 9,0);";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":contents", $contents, PDO::PARAM_STR);
+        $contents = $_POST['questionText'];
+        $stmt->execute();
+        header("Refresh:0");
+    }
 ?>
 <link rel="stylesheet" href="questionAppearance.css">
 <!DOCTYPE html>
@@ -39,25 +57,57 @@
 <body>
 <div class="container">
     <div class="row">
-        <form method="post" class="newForm">
-            <!--Input Boxes to change Form name and description -->
+        <form method="POST">
+        <?php
+            foreach ($questions as $row) 
+            {
+                echo $row['question number'];
+                echo $row['Contents'];
+                echo "<br>";
+        ?>
+        <button class="btn btn-primary" name="<?php echo $row['ID'] ?>" value="<?php echo $row['ID'] ?>">Delete</button>
+        <?php 
+            }
+        ?>
+        </form>
+        <form method="POST">
+            <input type="text" name="questionText">
+            <button type="submit" name="submitQuestions">Submit</button>
+        </form>
+        <div class="card">
+            <div class="card-body">
+                <form name="textQuestion" method="POST">
+                    <input type = "text" id =' + questionNumber +' name="myText" placeholder="Enter question here:" class="card-title question-title"/>
+                    <p class="card-text"><em>Answer here</em></p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button value="submit" name="submit" id ="submitQuestion" onclick="tickButton()">Submit</button>
+                        <a href="#" id = ' + questionNumber + ' onclick="deleteCard(this.id)" class="btn btn-primary btn-danger fas fa-trash"></a>
+                    </div>
+                    <div class="form-check"><input name = "required" type="checkbox" class="form-check-input" id="required">
+                        <label class="form-check-label" for="required">Required</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- <form method="post" class="newForm">
+            
             <input type="text" id="formName" value="<?php echo $title ?>" class="card-title question-title" name="formName"/>
             <input name = "formDesc" type="text" id="formDescription"  value="<?php echo $description ?>"
                    class="card-title question-title"/>
-            <!--All question/cards will be placed here when append-->
+           
             <div id="questionPanel">
 
             </div>
-            <!-- Button trigger modal -->
+            
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" id="newQuestion">
                 Add New Question
             </button>
             <button type="submit" class="btn btn-primary" name="submitForm" method="post" id="submitForm"> Create Form
             </button>
-        </form>
+        </form>-->
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    
+<!--     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
@@ -71,7 +121,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <div class="row justify-content-end" id="imageRow">
       <img src="logo.png" class="fix">
     </div>
@@ -84,9 +134,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
         crossorigin="anonymous"></script>
@@ -94,4 +141,3 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/8741ca18b0.js" crossorigin="anonymous"></script>
-<script src="addQuestions.js"></script>
