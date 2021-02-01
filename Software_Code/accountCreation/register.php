5e75a7c49288ca57bff72f28bfd9ed10af69e42f
fincharry27 <harry.gfinch@gmail.com>
2021-02-01 15:15:33 +0000

    added php for account creation and taken username checking

--- Software_Code/accountCreation/register.php
+++ Software_Code/accountCreation/register.php
@@ -1,4 +1,48 @@
 <!doctype html>
+<?php 
+
+require('../conn.php');
+session_start();
+
+$username = "";
+$username_err = "";
+
+if (isset($_POST['submitAccount'])) 
+{
+	$query = "SELECT ID FROM account WHERE account.`Username` = :username;";
+	$stmt = $pdo->prepare($query);
+	$stmt->bindParam(":username", $username, PDO::PARAM_STR);
+	$username = $_POST['username'];
+	$stmt->execute();
+	if ($stmt->rowCount() == 1) 
+	{
+		$username_err = "This username is already taken";
+	}
+	else
+	{
+		$username = $_POST['username'];
+	}
+
+	if (empty($username_err)) 
+	{
+		$query = "INSERT INTO account (`Name`,`Username`,`Password`,`Role`) VALUES (:name,:username,:password,'researcher')";
+
+		$stmt = $pdo->prepare($query);
+
+		$stmt->bindParam(":name", $name, PDO::PARAM_STR);
+		$stmt->bindParam(":username", $username, PDO::PARAM_STR);
+		$stmt->bindParam(":password", $password, PDO::PARAM_STR);
+
+		$name = $_POST['name'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$stmt->execute();
+
+	}
+}
+
+?>
 <html lang="en">
 
   <head>
@@ -13,19 +57,17 @@
 
   <body class="text-center">
     <div class="container">
-      <form class="form-signin">
+      <form class="form-signin" method="POST">
         <img class="mb-4" src="../images/University_Of_Dundee_shield.png" alt="" width="72" height="72">
+
         <h1 class="h3 mb-3 font-weight-normal">Please register with Dundata</h1>
-        <label for="inputName" class="sr-only">Name</label>
-        <input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
 
-        <label for="inputUsername" class="sr-only">Username</label>
-        <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <input type="text" name="name" id="inputName" class="form-control" placeholder="Name" required autofocus>
+        <input type="text" name="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <span class="help-block"><?php echo $username_err; ?></span>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="submitAccount">Sign up</button>
       </form>
     </div>
   </body>
