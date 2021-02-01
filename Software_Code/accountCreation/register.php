4fbb50924d97fd7550d0a4ac3bcaebb24feaa1a8
James Robb <43144223+JamesRobb03@users.noreply.github.com>
2021-02-01 14:30:07 +0000

    register.php page

---
+++ Software_Code/accountCreation/register.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+    <title>Register Now</title>
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico"/>
+  </head>
+
+  <body class="text-center">
+    <div class="container">
+      <form class="form-signin">
+        <img class="mb-4" src="../images/University_Of_Dundee_shield.png" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Please register with Dundata</h1>
+        <label for="inputName" class="sr-only">Name</label>
+        <input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
+
+        <label for="inputUsername" class="sr-only">Username</label>
+        <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+      </form>
+    </div>
+  </body>
+
+</html>
