5679f5a12fb1abef90d28d7b2166a8524a58a7c4
Luke <luke.dawe@gmail.com>
2021-02-01 15:33:30 +0000

    Edited the layout of the page.

--- Software_Code/videoStreams/syncStreams.php
+++ Software_Code/videoStreams/syncStreams.php
@@ -14,55 +14,56 @@ date_default_timezone_set('Europe/London');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../CSS/style.css">
     <title>Dundata</title>
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico"/>
 </head>
 
 <body>
-    <nav class="navbar navbar-dark">
-        <a class="navbar-brand" href="../index.html">
-            <img src="../images/University_of_Dundee_shield_white.png" width="27" height="37" alt="Uni Logo"
-                 style="margin-right: 20px;">Home
-        </a>
-    </nav>
+<nav class="navbar navbar-dark">
+    <a class="navbar-brand" href="../index.html">
+        <img src="../images/University_of_Dundee_shield_white.png" width="27" height="37" alt="Uni Logo"
+             style="margin-right: 20px;">Home
+    </a>
+</nav>
 
-    <div class="container">
-        <div class="row">
-            <h1><u>Sync Video Streams</u></h1>
-        </div>
-        <div class="row" id="imageRow">
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                        <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload1">
-                        <video width="300" id="player1" controls></video>
-                    </div>
-                    <div class="col-sm">
-                        <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload2">
-                        <video width="300" id="player2" controls></video>
-                    </div>
-                    <div class="col-sm">
-                        <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload3">
-                        <video width="300" id="player3" controls></video>
-                    </div>
-                </div>
-                <button class="fas fa-play btn btn-primary" onclick="playAll()" id="playButton" ></button>
-                <button class="fas fa-flag btn btn-primary" onclick="flag()" id="flagButton" ></button>
-            </div>
-
-            <!--
-                Upload a video and play it.
-            https://developers.google.com/web/fundamentals/media/recording-video
-            -->
-
-        </div>
-        <div class="row justify-content-end" id="imageRow">
-            <img src="../images/logo.png" class="fix">
-        </div>
+<div class="container">
+    <div class="row">
+        <h1><u>Sync Video Streams</u></h1>
     </div>
+    <div class="row" id="imageRow">
+
+
+        <div class="col-sm">
+            <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload1">
+            <video width="100%" id="player1" controls></video>
+        </div>
+        <div class="col-sm">
+            <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload2">
+            <video width="100%" id="player2" controls></video>
+        </div>
+        <div class="col-sm">
+            <input class="form-control" type="file" accept="video/*" capture="camera" id="fileUpload3">
+            <video width="100%" id="player3" controls></video>
+        </div>
+
+        <div>
+            <button class="fas fa-play btn btn-primary" onclick="playAll()" id="playButton"></button>
+            <button class="fas fa-flag btn btn-primary" onclick="flag()" id="flagButton"></button>
+        </div>
+
+
+        <!--
+            Upload a video and play it.
+        https://developers.google.com/web/fundamentals/media/recording-video
+        -->
+
+    </div>
+    <div class="row justify-content-end" id="imageRow">
+        <img src="../images/logo.png" class="fix">
+    </div>
+</div>
 </body>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="https://kit.fontawesome.com/8741ca18b0.js" crossorigin="anonymous"></script>
@@ -71,4 +72,4 @@ date_default_timezone_set('Europe/London');
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
 
 
-</html>
+    </html>
