a7df9d91824e94f2c0564283415a238ba877ec26
AlanSunny7509 <77677465+AlanSunny7509@users.noreply.github.com>
2021-02-01 13:22:00 +0000

    Ethics_stuff

--- Software_Code/ethicsJS.js
+++ Software_Code/ethicsJS.js
@@ -1,5 +1,5 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
+ const closeModalButtons = document.querySelectorAll('[data-close-button]') 
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
