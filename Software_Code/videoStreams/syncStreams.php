bd6d42de0dccda65691c0eec1ff1ac9a86255fe5
Luke <luke.dawe@gmail.com>
2021-02-03 13:58:01 +0000

    Removed repeated code, made one method to add event listeners for the videos.

--- Software_Code/videoStreams/syncStreams.php
+++ Software_Code/videoStreams/syncStreams.php
@@ -102,5 +102,4 @@ date_default_timezone_set('Europe/London');
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
 
-
     </html>
